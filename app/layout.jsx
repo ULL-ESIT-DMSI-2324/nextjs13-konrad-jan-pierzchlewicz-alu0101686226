6e69364fabc0b2e7c54509af9c8cb7d840f3b2d1
Konobono <129867527+Konobono@users.noreply.github.com>
2023-12-18 10:45:18 +0000

    Lesson progress

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Link from '/next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <nav>
+          <h1>Dojo Helpdesk</h1>
+          <Link href="/tickets">Tickets</Link>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
