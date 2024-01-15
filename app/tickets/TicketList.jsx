f974097838095ae13e14db15c32476d9bc4ed261
Konobono <129867527+Konobono@users.noreply.github.com>
2024-01-15 14:54:07 +0000

    Content of learn-7

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 async function getTickets() {
     const response = await fetch('http://localhost:4000/tickets', {
@@ -8,21 +9,25 @@ async function getTickets() {
     const data = await response.json();
     return data;
 }
-
 export default async function TicketList() {
     //fetch data from api
     const tickets = await getTickets();
   return (
     <>
-        {tickets.map(ticket => (
+        {tickets.map((ticket) => (
             <div key={ticket.id} className="card my-5">
-                <h3>{ticket.title}</h3>
-                <p>{ticket.body.slice(0, 200)}...</p>
-                <div className= {`pill ${ticket.priority} `}></div>
-                {ticket.priority} priority
+                <Link href={`/tickets/@{ticket.id}`}>
+                    <h3>{ticket.title}</h3>
+                    <p>{ticket.body.slice(0, 200)}...</p>
+                    <div className= {`pill ${ticket.priority} `}>
+                        {ticket.priority} priority
+                    </div>
+                </Link>
             </div>
         ))}
-        {tickets.length === 0 && <p>No tickets to show</p>}
+        {tickets.length === 0 && (
+            <p className= "text-center" > There are no open tickets, geronimo!</p>
+        )}
     </>
   )
 }
